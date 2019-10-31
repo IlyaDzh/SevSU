a6d79c4fe42b4e6f748716f5a45e6b5a6f959e21
Илья Долженко <ilay00@mail.ru>
2019-10-31 19:21:10 +0300

    ТПО 3

---
+++ 3 курс/5 семестр/ТПО 3/ConsoleApp3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = new NumSymbolTester();
+            Log.Close();
+        }
+    }
+
+    public class NumberSymbol
+    {
+        public Tuple<int, int> CheckString(string str)
+        {
+            int countForwardSlash = 0;
+            int countExclamationMark = 0;
+            foreach (var ch in str)
+            {
+                if (ch == '/')
+                    ++countForwardSlash;
+                else if (ch == '!')
+                    ++countExclamationMark;
+            }
+
+            return new Tuple<int, int>(countForwardSlash, countExclamationMark);
+        }
+    }
+
+    public class Log
+    {
+        private static readonly StreamWriter log = new StreamWriter("result.txt");
+        static public void AddString(string s)
+        {
+            log.WriteLine(s);
+        }
+        static public void Close()
+        {
+            log.Close();
+        }
+    }
+
+    abstract class Tester
+    {
+        protected void LogMessage(string s)
+        {
+            Log.AddString(s);
+        }
+    }
+
+    class NumSymbolTester : Tester
+    {
+        private readonly NumberSymbol OUT;
+        public NumSymbolTester()
+        {
+            OUT = new NumberSymbol();
+            Run();
+        }
+        private void Run()
+        {
+            StringTest1();
+        }
+        private void StringTest1()
+        {
+            string[] strings = {
+                "Тест",
+                "!",
+                "/",
+                "Тест /!/ тест",
+                "Тест!",
+                "!/Тест",
+                "///!//!!!!/////!!!!!!!!!!!!!!///!/"
+            };
+
+            foreach (var str in strings)
+            {
+                var result = OUT.CheckString(str);
+                Log.AddString($"Строка: {str}");
+                Log.AddString($"Количество символов /: {result.Item1}");
+                Log.AddString($"Количество символов !: {result.Item2}");
+                Log.AddString("------------------------------------------");
+            }
+        }
+    }
+}
