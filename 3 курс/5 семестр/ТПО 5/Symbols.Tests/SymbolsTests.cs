3bb716ca4bc7a8490ff862d8b8de1530577a6bc1
Илья Долженко <ilay00@mail.ru>
2019-11-22 20:41:14 +0300

    ТПО 5

---
+++ 3 курс/5 семестр/ТПО 5/Symbols.Tests/SymbolsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SymbolsLib.Tests
+{
+    [TestClass]
+    public class SymbolsTests
+    {
+        readonly string[] strings = {
+            "Тест",
+            "!",
+            "/",
+            "Тест /!/ тест",
+            "Тест!",
+            "!/Тест",
+            "///!//!!!!/////!!!!!!!!!!!!!!///!/"
+        };
+        readonly Tuple<int, int>[] results = {
+            Tuple.Create(0, 0),
+            Tuple.Create(0, 1),
+            Tuple.Create(1, 0),
+            Tuple.Create(2, 1),
+            Tuple.Create(0, 1),
+            Tuple.Create(1, 1),
+            Tuple.Create(14, 20)
+        };
+
+        [TestMethod]
+        public void TestNumber1()
+        {
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Symbols numbers = new Symbols(strings[i]);
+                Tuple<int, int> output = numbers.CheckAndPrint();
+                Assert.AreEqual(output.Item1, results[i].Item1);
+                Assert.AreEqual(output.Item2, results[i].Item2);
+            }
+        }
+
+        [TestMethod]
+        public void TestNumber2()
+        {
+            for (int i = 0; i < strings.Length; i++)
+            {
+                Symbols numbers = new Symbols(null);
+                numbers.Check(strings[i]);
+                Assert.AreEqual(numbers.countForwardSlash, results[i].Item1);
+                Assert.AreEqual(numbers.countExclamationMark, results[i].Item2);
+            }
+        }
+    }
+}
---
+++ 3 курс/5 семестр/ТПО 5/SymbolsLib/Symbols.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SymbolsLib
+{
+    public class Symbols
+    {
+        private string str;
+        public int countForwardSlash = 0;
+        public int countExclamationMark = 0;
+
+        public Symbols(string _str)
+        {
+            str = _str;
+        }
+
+        public Tuple<int, int> CheckAndPrint()
+        {
+            Check(str);
+
+            Console.WriteLine("Количество символов / в строке: " + countForwardSlash);
+            Console.WriteLine("Количество символов ! в строке: " + countExclamationMark);
+
+            return new Tuple<int, int>(countForwardSlash, countExclamationMark);
+        }
+
+        public void Check(string str)
+        {
+            foreach (var ch in str)
+            {
+                if (ch == '/')
+                    ++countForwardSlash;
+                else if (ch == '!')
+                    ++countExclamationMark;
+            }
+        }
+    }
+}
