f7582a80eb15423116e30f28d2d9d2c6b081a2d7
Илья Долженко <ilay00@mail.ru>
2020-08-20 15:35:57 +0300

    УИР 3

---
+++ 3 курс/6 семестр/УИР 3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewFile = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewHash = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxSubscriber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxSearchSubscriber = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewFile
+            // 
+            this.dataGridViewFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFile.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewFile.Location = new System.Drawing.Point(17, 45);
+            this.dataGridViewFile.MultiSelect = false;
+            this.dataGridViewFile.Name = "dataGridViewFile";
+            this.dataGridViewFile.Size = new System.Drawing.Size(524, 340);
+            this.dataGridViewFile.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Абонент";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Телефон";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Дом";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 80;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Сумма";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 80;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Данные из файла";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(570, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hash-таблица";
+            // 
+            // dataGridViewHash
+            // 
+            this.dataGridViewHash.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHash.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.dataGridViewHash.Location = new System.Drawing.Point(574, 45);
+            this.dataGridViewHash.MultiSelect = false;
+            this.dataGridViewHash.Name = "dataGridViewHash";
+            this.dataGridViewHash.Size = new System.Drawing.Size(375, 340);
+            this.dataGridViewHash.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Hash";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 70;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Ключ";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 260;
+            // 
+            // textBoxSubscriber
+            // 
+            this.textBoxSubscriber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSubscriber.Location = new System.Drawing.Point(17, 414);
+            this.textBoxSubscriber.Name = "textBoxSubscriber";
+            this.textBoxSubscriber.Size = new System.Drawing.Size(176, 22);
+            this.textBoxSubscriber.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(14, 395);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Абонент";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(196, 396);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Телефон";
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPhone.Location = new System.Drawing.Point(199, 414);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(130, 22);
+            this.textBoxPhone.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(332, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Дом";
+            // 
+            // textBoxHome
+            // 
+            this.textBoxHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxHome.Location = new System.Drawing.Point(335, 414);
+            this.textBoxHome.Name = "textBoxHome";
+            this.textBoxHome.Size = new System.Drawing.Size(100, 22);
+            this.textBoxHome.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(438, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Сумма";
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSum.Location = new System.Drawing.Point(441, 414);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSum.TabIndex = 10;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(17, 442);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(86, 23);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(107, 442);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(86, 23);
+            this.buttonRemove.TabIndex = 13;
+            this.buttonRemove.Text = "Удалить";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(13, 510);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 24);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Поиск";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(14, 550);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Абонент";
+            // 
+            // textBoxSearchSubscriber
+            // 
+            this.textBoxSearchSubscriber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSearchSubscriber.Location = new System.Drawing.Point(17, 570);
+            this.textBoxSearchSubscriber.Name = "textBoxSearchSubscriber";
+            this.textBoxSearchSubscriber.Size = new System.Drawing.Size(176, 22);
+            this.textBoxSearchSubscriber.TabIndex = 15;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(199, 570);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(86, 23);
+            this.buttonSearch.TabIndex = 17;
+            this.buttonSearch.Text = "Найти";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(291, 570);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(86, 23);
+            this.buttonReset.TabIndex = 18;
+            this.buttonReset.Text = "Сбросить";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(570, 396);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 24);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Лог";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Enabled = false;
+            this.textBoxLog.Location = new System.Drawing.Point(574, 423);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(375, 170);
+            this.textBoxLog.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(964, 612);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxSearchSubscriber);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxSum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxHome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxSubscriber);
+            this.Controls.Add(this.dataGridViewHash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewFile);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hashtable";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHash)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewFile;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewHash;
+        private System.Windows.Forms.TextBox textBoxSubscriber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxSearchSubscriber;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ 3 курс/6 семестр/УИР 3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private readonly string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\file.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            readFile();
+            dataGridViewHash.Rows.Add("0", "Бюро расписаний");
+            dataGridViewHash.Rows.Add("1", "Бюро пропусков");
+        }
+
+        private void readFile()
+        {
+            dataGridViewFile.Rows.Clear();
+            string[] lines = File.ReadAllLines(path);
+            string[] values;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Split('\t');
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j < values.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+                dataGridViewFile.Rows.Add(row);
+            }
+        }
+
+        //private void writeFile()
+        //{
+        //    StreamWriter writer = new StreamWriter(path);
+        //    for (int i = 0; i < dataGridViewFile.Rows.Count - 1; i++)
+        //    {
+        //        for (int j = 0; j < dataGridViewFile.Columns.Count; j++)
+        //        {
+        //            writer.Write(dataGridViewFile.Rows[i].Cells[j].Value.ToString() + "\t");
+        //        }
+        //        writer.WriteLine("");
+        //    }
+        //    writer.Close();
+        //}
+
+        private void appendToFile(string[] row)
+        {
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    writer.Write(row[i] + "\t");
+                }
+                writer.WriteLine("");
+            }
+        }
+
+        private void resetAllFields()
+        {
+            textBoxSubscriber.Text = "";
+            textBoxPhone.Text = "";
+            textBoxHome.Text = "";
+            textBoxSum.Text = "";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string[] row = new string[] { textBoxSubscriber.Text, textBoxPhone.Text, textBoxHome.Text, textBoxSum.Text };
+
+            dataGridViewFile.Rows.Add(row);
+            appendToFile(row);
+
+            resetAllFields();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines(path);
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split('\t');
+                    if (words[0] != textBoxSubscriber.Text)
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+
+            readFile();
+            resetAllFields();
+        }
+    }
+}
