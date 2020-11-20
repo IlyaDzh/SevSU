601853180b9890d2fc0eeff9ed408584c090de76
Илья Долженко <ilay00@mail.ru>
2020-11-20 21:59:54 +0300

    ТРСиПВ 2

--- 4 курс/7 семестр/ТРСиПВ 2/ConsoleApplication1/ConsoleApplication1.cpp
+++ 4 курс/7 семестр/ТРСиПВ 2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,167 +1,99 @@
 ﻿#include <iostream>
-#include <fstream>
+#include <iomanip>
 #include "mpi.h"
 
 using namespace std;
 
-double** InitMatrix(int row, int column);
-double** ReadMatrixFromFile(string path, int* row, int* column);
-void PrintMatrix(double** matrix, int row, int column);
-double* fromMatrixToVector(double** matrix, int row, int column);
-double** fromVectorToMatrix(double* vector, int row, int column);
-double* MatrixMultyply(double** partMatrixA, int rowA, int columnA, double** matrixB, int rowB, int columnB);
-double* MultiplyRowsByColumn(double* vectorA, double** matrixB, int vector_column, int matrix_column);
+const int N = 8;
+const int M = 5;
+const int K = 3;
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-	int rowA = 8, columnA = 5, rowB = 5, columnB = 3, buffSize = 5 * 3;
-
-	MPI_Status status;
-	int rank, size, i = 0, k = 2;
-	double** matrixA, ** matrixB, * buffer = new double[buffSize];
-	double* mainResult = new double[rowA * columnB];
-	double* partResult = new double[k * columnB];
-	double* vectorToSend = new double[(rowA / k) * columnA * k];
-	double* recvBuffer = new double[k * columnA];
-
+	int rank, processes;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &processes);
+	int masterProcess = 0;
 
-	if (rank == 0) {
-		// 1) рассылаем matrixA по 2 строки по процессам
-		matrixA = ReadMatrixFromFile("matrixA.txt", &rowA, &columnA);
-		cout << "Matrix A:" << endl;
-		PrintMatrix(matrixA, rowA, columnA);
+	int A[N][M];
+	int B[M][K];
+	int C[N][K];
+	int blockA[2][M];
+	int blockC[2][K];
 
-		// создаем из каждых двух строк матрицы один вектор
-		double** matrixToSender = InitMatrix(rowA / k, columnA * k);
-		for (int i = 0, number = 0; i < rowA; number++) {
-			double** temp = InitMatrix(k, columnA);
-			for (int j = 0; j < k; j++) {
-				temp[j] = matrixA[i++];
+	if (rank == masterProcess)
+	{
+		int dataA[N][M] = {
+			{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+			{26, 27, 28, 29, 30},
+			{31, 32, 33, 34, 35},
+			{36, 37, 38, 39, 40},
+		};
+
+		int dataB[M][K] = {
+			{1, 2, 3},
+			{4, 5, 6},
+			{7, 8, 9},
+			{10, 11, 12},
+			{13, 14, 15},
+		};
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < K; j++) {
+				B[i][j] = dataB[i][j];
 			}
-			matrixToSender[number] = fromMatrixToVector(temp, k, columnA);
 		}
-		vectorToSend = fromMatrixToVector(matrixToSender, rowA / k, columnA * k);
 
-		// создаем из матрицы matrixB вектор buffer
-		matrixB = ReadMatrixFromFile("matrixB.txt", &rowB, &columnB);
-		cout << "Matrix B: " << endl;
-		PrintMatrix(matrixB, rowB, columnB);
-
-		buffer = fromMatrixToVector(matrixB, rowB, columnB);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				A[i][j] = dataA[i][j];
+			}
+		}
 	}
 
-	// отправляем каждому процессу свою часть матрицы А в виде вектора
-	MPI_Scatter(vectorToSend, k * columnA, MPI_DOUBLE, recvBuffer, k * columnA, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// Широковещательная рассылка
+	MPI_Bcast(B, M * K, MPI_INT, masterProcess, MPI_COMM_WORLD);
+	MPI_Scatter(A, 10, MPI_INT, blockA, 10, MPI_INT, masterProcess, MPI_COMM_WORLD);
 
-	// 2) рассылаем matrixB по всем процессам
-	MPI_Bcast(buffer, buffSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// Вычисления
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < K; j++) {
+			blockC[i][j] = 0;
+			for (int g = 0; g < M; g++) {
+				blockC[i][j] += blockA[i][g] * B[g][j];
+			}
+		}
+	}
 
-	// 3) в каждом процессе получаем свою часть матрицы А и всю матрицу В в виде векторов
-	double** partMatrixA = fromVectorToMatrix(recvBuffer, k, columnA);
-	matrixB = fromVectorToMatrix(buffer, rowB, columnB);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < K; j++) {
+			cout << setw(6) << blockC[i][j] << " ---" << rank << "---";
+		}
+		cout << endl;
+	}
 
-	partResult = MatrixMultyply(partMatrixA, k, columnA, matrixB, rowB, columnB);
-	cout << "Result of matrix multiplication: " << endl;
-	PrintMatrix(fromVectorToMatrix(partResult, k, columnB), k, columnB);
-
-	// 4) собираем результат умножения со всех процессов
+	// Взаимная синхронизация
 	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Gather(partResult, k * columnB, MPI_DOUBLE, mainResult, k * columnB, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-	if (rank == 0) {
-		cout << "Result: " << endl;
-		PrintMatrix(fromVectorToMatrix(mainResult, rowA, columnB), rowA, columnB);
+	// Совместная передача данных процессу-мастеру
+	MPI_Gather(blockC, 6, MPI_INT, C, 6, MPI_INT, masterProcess, MPI_COMM_WORLD);
+
+	if (rank == masterProcess) {
+		cout << endl << endl << "Result matrix: " << endl;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < K; j++) {
+				cout << setw(6) << C[i][j] << " ";
+			}
+			cout << endl;
+		}
 	}
 
 	MPI_Finalize();
 
 	return 0;
-}
-
-double** InitMatrix(int row, int column) {
-	double** matrix = new double* [row];
-	for (int i = 0; i < row; i++) {
-		matrix[i] = new double[column];
-	}
-	return matrix;
-}
-
-double** ReadMatrixFromFile(string path, int* row, int* column) {
-	ifstream in(path);
-	double** matrix;
-
-	if (in.is_open()) {
-		in >> *row >> *column;
-
-		matrix = InitMatrix(*row, *column);
-
-		for (int i = 0; i < *row; i++) {
-			for (int j = 0; j < *column; j++) {
-				in >> matrix[i][j];
-			}
-		}
-
-		in.close();
-		return matrix;
-	}
-	else {
-		cout << "Файл не найден!";
-		return 0;
-	}
-}
-
-void PrintMatrix(double** matrix, int row, int column) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++) {
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-
-double* fromMatrixToVector(double** matrix, int row, int column) {
-	double* buffer = new double[row * column];
-	int k = 0;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++, k++) {
-			buffer[k] = matrix[i][j];
-		}
-	}
-	return buffer;
-}
-
-
-double** fromVectorToMatrix(double* vector, int row, int column) {
-	double** result_matrix = InitMatrix(row, column);
-	int index = 0;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++) {
-			result_matrix[i][j] = vector[index++];
-		}
-	}
-	return result_matrix;
-}
-
-
-double* MatrixMultyply(double** partMatrixA, int rowA, int columnA, double** matrixB, int rowB, int columnB) {
-	double** result_multiplication = InitMatrix(rowA, columnB);
-	for (int i = 0; i < rowA; i++) {
-		result_multiplication[i] = MultiplyRowsByColumn(partMatrixA[i], matrixB, columnA, columnB);
-	}
-	return fromMatrixToVector(result_multiplication, rowA, columnB);
-}
-
-
-double* MultiplyRowsByColumn(double* vectorA, double** matrixB, int vector_column, int matrix_column) {
-	double* result_vector = new double[vector_column] {0};
-	for (int i = 0; i < matrix_column; i++) {
-		for (int t = 0; t < vector_column; t++) {
-			result_vector[i] += vectorA[t] * matrixB[t][i];
-		}
-	}
-	return result_vector;
 }
